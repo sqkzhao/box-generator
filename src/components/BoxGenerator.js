077296e02b26bf126f0a919b13f773a2ce455094
Kay Zhao <sqkzhao@gmail.com>
2020-03-04 19:46:51 -0800

    added height/width input

--- src/components/BoxGenerator.js
+++ src/components/BoxGenerator.js
@@ -4,6 +4,7 @@ import StyledBox from './StyledBox'
 const BoxGenerator = props => {
     const [boxState, setBoxState] = useState({
         color: "",
+        height: "",
         boxes: []
     })
     const onChangeHandler = (e) => {
@@ -15,21 +16,24 @@ const BoxGenerator = props => {
     const submitHandler = (e) => {
         e.preventDefault()
         setBoxState({
-            boxes: boxState.boxes.concat(boxState.color),
-            color: ""
+            boxes: boxState.boxes.concat({color: boxState.color, height: boxState.height}),
+            color: "",
+            height: ""
         })
     }
     return (
         <div className="container mt-5">
-            <form onSubmit={submitHandler} className="col-5 mx-auto">
-                <label>COLOR</label>
+            <form onSubmit={submitHandler} className="col-6 mx-auto">
+                <label className="col-4">Color</label>
                 <input type="text" name="color" value={boxState.color} onChange={onChangeHandler}/>
-                {/* <input type="text" name="height" value={boxState.height} onChange={onChangeHandler}/> */}
+                <br></br>
+                <label className="col-4">Height/Width</label>
+                <input type="number" name="height" value={boxState.height} onChange={onChangeHandler}/>
                 <input type="submit" value="Add"/>
             </form>
             <div className="col-6 mx-auto mt-3">
                 {boxState.boxes.map((box, i) => 
-                    <StyledBox key={i} bgColor={box}/>
+                    <StyledBox key={i} bgColor={box.color} height={box.height}/>
                 )}
             </div>
         </div>
--- src/components/StyledBox.js
+++ src/components/StyledBox.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledBox = styled.div`
-    width: 120px;
-    height: 120px;
+    width: ${props => props.height || '120'}px;
+    height: ${props => props.height || '120'}px;
     background: ${props => props.bgColor};
     display: inline-block;
     margin-right: 5px;
